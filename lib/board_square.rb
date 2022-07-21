c437edc0620956cfa46da6cb07dc5da714cd9a21
Pierre-François Salmand <maabaa@gmail.com>
2022-07-21 16:28:19 +0700

    Finish BoardSquare candidate
    Co-authored-by: Sam Linard <Samsou1@users.noreply.github.com>

--- lib/board_square.rb
+++ lib/board_square.rb
@@ -1,3 +1,27 @@
-class BoardSquare
+require_relative 'board'
 
+# This class deals with single board squares
+class BoardSquare
+  attr_accessor :value, :allocated, :fill_circle, :fill_cross
+
+  def initialize
+    @value = nil
+    @allocated = check_allocated
+    @fill_circle = write_circle
+    @fill_cross = write_cross
+  end
+
+  def check_allocated
+    @value.is_a?(String) ? true : false
+  end
+
+  def write_circle
+    @value = 'O'
+  end
+
+  def write_cross
+    @value = 'X'
+  end
 end
+
+puts BoardSquare.new.check_allocated

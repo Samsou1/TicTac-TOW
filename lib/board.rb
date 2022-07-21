6767acf5dff34c1b591511f227e0df97545d7ae1
Pierre-François Salmand <maabaa@gmail.com>
2022-07-21 16:34:00 +0700

    Finish Board candidate

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,20 @@
-# This class deals with displaying the board
+require_relative 'board_square'
+
+# This class deals with managing the board
 class Board
-  attr_accessor :squares, :crosses, :circles
+  attr_accessor :squares, :crosses, :circles, :full
 
   def initialize
-    @squares = (1..9).to_a
+    @squares = []
+    9.times { @squares << BoardSquare.new }
     @crosses = []
+    9.times { @crosses << nil }
     @circles = []
+    9.times { @circles << nil }
+    @full = check_full?
+  end
+
+  def check_full?
+    @squares.all? { |el| el.is_a?(String) } ? true : false
   end
 end
-
-# print Board.new.squares

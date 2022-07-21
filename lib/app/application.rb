47c2dd1b5add2492f0b4bdd46454e5a496f1b9d0
Pierre-François Salmand <maabaa@gmail.com>
2022-07-21 20:07:28 +0700

    RC1

--- lib/app/application.rb
+++ lib/app/application.rb
@@ -1,55 +1,89 @@
 require_relative '../game'
+require_relative '../player'
 
+# This class performs the game loop
 class Application
+  attr_accessor :name1, :name2, :game_won1, :game_won2, :game_drawn
 
   def initialize
-    puts 'Welcome to Tictac TOE!'
-    puts "Player 1, what's your name?"
-    name1 = gets.chomp
+    @name1 = ''
+    @name2 = ''
+    @game_won1 = 0
+    @game_won2 = 0
+    @game_drawn = 0
+    screen_clear
+    prompt_welcome
+    prompt_name
+    prompt_rules
+    game_loop
+  end
+
+  def screen_clear
+    system('clear')
+  end
+
+  def prompt_welcome
+    puts "\n\nWelcome to Tictac TOE!"
+  end
+
+  def prompt_name
+    puts "\nPlayer 1, what's your name?"
+    @name1 = gets.chomp
     puts "That's a great name #{name1}, how about you Player 2?"
-    name2 = gets.chomp
+    @name2 = gets.chomp
     puts "Welcome to both of you #{name1} and #{name2}"
+  end
+
+  def prompt_rules
     puts "\nI'm sure you all know this game but just so we're clear, here are the rules again"
     puts 'Here is a board of 9 squares that are empty for now'
     puts "You will place your symbol, either a 'X' or a 'O' on each square"
     puts 'In order to win the game, you need to place 3 of your symbol on the same row, column or diagonal'
     puts "If all 9 squares are filled and there's no winner then it's a draw"
     puts 'Shall we?'
-    wanna_play = true
-    number_of_win_play1 = 0
-    number_of_win_play2 = 0
-    number_of_draws = 0
-    while wanna_play = true
-      game = Game.new(name1, name2)
-      number_of_win_play1 += 1 if game.check_if_win[1] == "x"
-      number_of_win_play2 += 1 if game.check_if_win[1] == "o"
-      number_of_draws += 1 if game.check_if_draw == true
-      puts "#{name1} won #{number_of_win_play1} times"
-      puts "#{name2} won #{number_of_win_play2} times"
-      puts "There has been #{number_of draws} draws"
-      if number_of_win_play1 > number_of_win_play2
-        puts "\n#{name1} is leading by #{number_of_win_play1 - number_of_win_play2} points"
-      end
-      if number_of_win_play2 > number_of_win_play1
-        puts "#{name2} is leading by #{number_of_win_play2 - number_of_win_play1} points"
-      end
-      puts "It's a tight race, you both have the same number of points " if number_of_win_play2 == number_of_win_play1
+  end
+
+  def display_stats
+    puts "#{@name1} won #{game_won1} times"
+    puts "#{@name2} won #{game_won2} times"
+    puts "There has been #{game_drawn} draws"
+    if game_won1 > game_won2
+      puts "\n#{@name1} is leading by #{@game_won1 - @game_won2} points"
+    elsif game_won2 > game_won1
+      puts "#{@name2} is leading by #{@game_won2 - @game_won1} points"
+    else
+      puts "It's a tight race, you both have the same number of points " if @game_won2 == @game_won1
       puts "\nDo you wanna play another game?"
-      acceptable_response = false
-      while acceptable_response == false
-        response = gets.chomp
-        if response.downcase == 'n' || response.downcase == 'no' || response.downcase == 'nah' || response.downcase == 'nope'
-          wanna_play = false
-          acceptable_response = true
-          puts 'Ok, see you another time!'
-        elsif response.downcase == 'y' || response.downcase == 'yes' || response.downcase == 'yeah' || response.downcase == 'yep'
-          wanna_play = true
-          puts "Awesome let's keep playing!"
-          acceptable_response = true
-        else
-          puts "Sorry, I didn't catch that, do you wanna keep playing?"
-        end
+    end
+  end
+
+  def keep_playing?
+    acceptable_response = false
+    while acceptable_response == false
+      response = gets.chomp
+      if response.downcase == 'n' || response.downcase == 'no' || response.downcase == 'nah' || response.downcase == 'nope'
+        puts 'Ok, see you another time!'
+        return false
+      elsif response.downcase == 'y' || response.downcase == 'yes' || response.downcase == 'yeah' || response.downcase == 'yep'
+        puts "Awesome let's keep playing!"
+        return true
+      else
+        puts "Sorry, I didn't catch that, do you wanna keep playing?"
       end
     end
   end
+
+  def game_loop
+    wanna_play = true
+    while wanna_play == true
+      game = Game.new(@name1, @name2)
+      @game_won1 += 1 if game.check_if_win[1] == 'X'
+      @game_won2 += 1 if game.check_if_win[1] == 'O'
+      @game_drawn += 1 if game.check_if_draw == true
+      display_stats
+      wanna_play = keep_playing?
+    end
+  end
 end
+
+Application.new
--- lib/game.rb
+++ lib/game.rb
@@ -3,41 +3,14 @@ require_relative 'board'
 
 # This i the main game loop
 class Game
-  attr_accessor :player1, :player2, :board
+  attr_accessor :player1, :player2, :board, :render
 
   def initialize(name1, name2)
     @player1 = Player.new(name1, 'X')
     @player2 = Player.new(name2, 'O')
     @board = Board.new
-    middle_game
-
-  end
-
-  def middle_game
-    while !check_if_win && !check_if_draw
-      puts "It's #{@player1}'s turn to play, where do you want to place your symbol?"
-      desired_play = gets.chomp
-      while legal_move?(desired_play) == false
-        "This is not possible, can you give me another move?"
-        desired_play = gets.chomp
-      end
-      if !check_if_win && !check_if_draw
-        puts "It's now #{@player2}'s turn to play, where do you want to place your symbol?"
-        desired_play = gets.chomp
-        while legal_move?(desired_play) == false
-          "This is not possible, can you give me another move?"
-          desired_play = gets.chomp
-        end
-      end
-    end
-    puts "The game is finished"
-  end
-
-  def legal_move?(str)
-    str = str.to_i
-    return false unless (str >= 1 && str <= 9) || @board[str - 1].class != Integer
-
-    true
+    @render = Render.new
+    play
   end
 
   def check_if_win
@@ -47,24 +20,66 @@ class Game
       [1, 5, 9], [3, 5, 7]
     ]
     winning_combination.each do |ary|
-      winning_element_for_x =0
+      winning_element_for_x = 0
       winning_element_for_o = 0
       ary.length.times do |index|
-        if  @board.crosses[ary[index]-1] == 'X'
+        if @board.grid[ary[index] - 1].value == 'X'
           winning_element_for_x += 1
-          return [true,'x'] if winning_element_for_x == 3
-        elsif @board.circles[ary[index]-1] == 'O'
+          return [true, 'x'] if winning_element_for_x == 3
+        elsif @board.grid[ary[index] - 1].value == 'O'
           winning_element_for_o += 1
-          return [true,'o'] if winning_element_for_o == 3
+          return [true, 'o'] if winning_element_for_o == 3
         end
       end
     end
-    [false,'']
+    [false, '']
+  end
+
+  def play
+    while !check_if_win[0] && !check_if_draw
+      puts "It's #{@player1.name}'s turn to play, where do you want to place your symbol?"
+      play_by_turn(1)
+      next unless !check_if_win[0] && !check_if_draw
+
+
+    end
+    puts 'The game is finished'
+    @render.render_board(@board.grid)
+  end
+
+  def play_by_turn(int)
+    if int == 1
+      @render.render_board(@board.grid)
+        desired_play = gets.to_i
+        while legal_move?(desired_play) == false
+          puts 'This is not possible, can you give me another move?'
+          @render.render_board(@board.grid)
+          desired_play = gets.to_i
+        end
+        @board.grid[desired_play - 1].write_cross
+      elsif int == 2
+        puts "It's now #{@player2.name}'s turn to play, where do you want to place your symbol?"
+        @render.render_board(@board.grid)
+        desired_play = gets.to_i
+        while legal_move?(desired_play) == false
+          puts 'This is not possible, can you give me another move?'
+          @render.render_board(@board.grid)
+          desired_play = gets.to_i
+        end
+        @board.grid[desired_play - 1].write_circle
+      else
+        puts "oulah problem"
+      end
+    end
+  end
+
+  def legal_move?(str)
+    str = str.to_i
+    (str >= 1 && str <= 9) && @board.grid[str - 1].value.is_a?(Integer) ? true : false
   end
 
   def check_if_draw
-    return true if !check_if_win[0] && @board.full
-    false
+    !check_if_win[0] && @board.check_full? ? true : false
   end
 
 end
